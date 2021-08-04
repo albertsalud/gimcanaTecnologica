cdb1a3b56b4187a4708d3e5633ff19e32c97caab
Albert Salud <albert.saludgarcia@cegedim.com>
2021-08-05 00:07:40 +0200

    Changes to set web URL as global variable

--- GimcanaTecnologica/src/main/java/tk/daudecinc/gimcana/config/BeansConfig.java
+++ GimcanaTecnologica/src/main/java/tk/daudecinc/gimcana/config/BeansConfig.java
@@ -1,11 +1,27 @@
 package tk.daudecinc.gimcana.config;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeansConfig {
+	
+	@Autowired
+	private ServletContext sc;
+	
+	@Value("${dd5.web.url}")
+	private String webURL;
+	
+	@PostConstruct
+	public void initGlobalParameters() {
+		sc.setAttribute("webURL", webURL);
+	}
 	
 	@Bean
 	public ModelMapper modelMapper() {
